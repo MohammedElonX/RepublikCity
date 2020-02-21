6539662efe921dd04d063515621ddf81cc16d3f7
Isiuaka Mohammed <mohammedelonx@gmail.com>
2020-02-21 17:54:36 +0100

    First commit with webView

---
+++ app/src/main/java/com/example/republikcity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.republikcity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView myWebView = new WebView(this);
+        setContentView(myWebView);
+        myWebView.loadUrl("https://therepublikcity.com");
+    }
+}
