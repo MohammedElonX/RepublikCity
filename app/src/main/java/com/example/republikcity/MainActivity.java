22b5adc220b8e8dd7230b03c21f265cc350b5eaf
Isiuaka Mohammed <mohammedelonx@gmail.com>
2020-02-21 22:20:23 +0100

    Removed app bar, added webView in ConstraintLayout

--- app/src/main/java/com/example/republikcity/MainActivity.java
+++ app/src/main/java/com/example/republikcity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.republikcity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -13,12 +14,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
         //Load webView
-        WebView myWebView = new WebView(this);
-        setContentView(myWebView);
+        WebView myWebView = findViewById(R.id.webview);
         myWebView.loadUrl(mUrl);
 
         //Navigation in webView
         myWebView.setWebViewClient(new WebViewClient());
+        myWebView.setWebChromeClient(new WebChromeClient());
+
+        //Enabling JavaScript in webView
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
     }
 }
