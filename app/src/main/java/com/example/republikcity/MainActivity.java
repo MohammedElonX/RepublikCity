c491bc593776321d3fe8eca25487643ce3ecdd9d
Isiuaka Mohammed <mohammedelonx@gmail.com>
2020-02-21 18:22:38 +0100

    Added webClient for inApp browsing

--- app/src/main/java/com/example/republikcity/MainActivity.java
+++ app/src/main/java/com/example/republikcity/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.republikcity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
         WebView myWebView = new WebView(this);
         setContentView(myWebView);
         myWebView.loadUrl("https://therepublikcity.com");
+
+        myWebView.setWebViewClient(new WebViewClient());
     }
 }
