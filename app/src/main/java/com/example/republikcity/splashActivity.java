a636b3b6f954e840756c4cb9b298051d69b90ed4
Isiuaka Mohammed <mohammedelonx@gmail.com>
2020-02-23 18:21:42 +0100

    Added splashActivity

---
+++ app/src/main/java/com/example/republikcity/splashActivity.java
@@ -0,0 +1,36 @@
+package com.example.republikcity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class splashActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN_TIME_OUT=3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //This method is used so that your splash activity
+        //can cover the entire screen.
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(splashActivity.this,
+                        MainActivity.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
