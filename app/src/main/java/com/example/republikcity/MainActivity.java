b561649b18c8bb2a60cd7a7a26822beffc15c21e
Isiuaka Mohammed <mohammedelonx@gmail.com>
2020-02-24 00:20:42 +0100

    Added custom webView client

--- app/src/main/java/com/example/republikcity/MainActivity.java
+++ app/src/main/java/com/example/republikcity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.republikcity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -29,8 +31,7 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setDomStorageEnabled(true);
 
         //Navigation in webView
-        myWebView.setWebViewClient(new WebViewClient());
-        myWebView.setWebChromeClient(new WebChromeClient());
+        myWebView.setWebViewClient(new MyWebViewClient());
     }
 
     @Override
@@ -44,6 +45,24 @@ public class MainActivity extends AppCompatActivity {
         // If it wasn't the Back key or there's no web page history, bubble up to the default
         // system behavior (probably exit the activity)
         return super.onKeyDown(keyCode, event);
+    }
+
+    public class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (Uri.parse(url).getHost().contains("therepublikcity")) {
+                // This is my website, so do not override; let my WebView load the page
+                return false;
+            }
+            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+            if(Uri.parse(url).getHost().contains("twitter") || Uri.parse(url).getHost().contains("facebook") ||
+                    Uri.parse(url).getHost().contains("instagram") || Uri.parse(url).getHost().contains("instagram") ||
+                    Uri.parse(url).getHost().contains("pinterest")){
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+            return true;
+        }
     }
 
 }
