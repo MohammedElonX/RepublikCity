8a7e9cd2912089e58140228281e946efb54e28a7
Isiuaka Mohammed <mohammedelonx@gmail.com>
2020-02-24 21:41:19 +0100

    Added swipeRefresh to webView

--- app/src/main/java/com/example/republikcity/MainActivity.java
+++ app/src/main/java/com/example/republikcity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.republikcity;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     private static String mUrl = "https://therepublikcity.com";
     //WebView
     private WebView myWebView;
+    private SwipeRefreshLayout mySwipeRefreshLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
 
         //Navigation in webView
         myWebView.setWebViewClient(new MyWebViewClient());
+
+        mySwipeRefreshLayout = findViewById(R.id.swipeContainer);
+        mySwipeRefreshLayout.setOnRefreshListener(
+                new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        myWebView.reload();
+                    }
+                }
+        );
     }
 
     @Override
@@ -52,16 +65,21 @@ public class MainActivity extends AppCompatActivity {
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             if (Uri.parse(url).getHost().contains("therepublikcity")) {
                 // This is my website, so do not override; let my WebView load the page
+
                 return false;
             }
             // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
             if(Uri.parse(url).getHost().contains("twitter") || Uri.parse(url).getHost().contains("facebook") ||
-                    Uri.parse(url).getHost().contains("instagram") || Uri.parse(url).getHost().contains("instagram") ||
-                    Uri.parse(url).getHost().contains("pinterest")){
+                    Uri.parse(url).getHost().contains("instagram") || Uri.parse(url).getHost().contains("pinterest")){
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
                 startActivity(intent);
             }
             return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            mySwipeRefreshLayout.setRefreshing(false);
         }
     }
 
